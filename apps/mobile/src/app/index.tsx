0cbbdea3e3b7066349876f689c8bc5f027490aab
JasonMisco11 <jasonmisco11@gmail.com>
2026-07-17 17:44:19 +0000

    test

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,6 +1,6 @@
 // import LottieView from "lottie-react-native";
 // import { MotiView } from "moti";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 // import sampleLottie from "../../assets/lottie/sample.json";
 
@@ -8,6 +8,8 @@ export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center gap-6 bg-slate-950 p-6">
       <Text className="text-xl font-semibold text-red-500">Asaase Mobile</Text>
+
+      {/* some way some how, this is causing issues, lottie */}
 
       {/* <MotiView
         from={{ opacity: 0, scale: 0.9 }}
@@ -22,6 +24,17 @@ export default function HomeScreen() {
         loop
         style={{ width: 120, height: 120 }}
       /> */}
+
+      <TouchableOpacity
+        onPress={() => {
+          console.log("Button pressed");
+        }}
+        className="rounded-lg bg-yellow-400 px-4 py-2"
+      >
+        <Text className="text-white">Me</Text>
+        
+      </TouchableOpacity>
+      
     </View>
   );
 }
